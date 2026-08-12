e07c929368607a306a3dda1f88b9164143c506c7
Frank Monteon <frank.monteon@gmail.com>
2026-08-11 20:09:05 -0600

    changed name to life notes

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -78,7 +78,7 @@ function buildNav() {
     <ul class="nav">
       <li class="nav-item"><a class="nav-link" href="index.html">Main Page</a></li>
       <li class="nav-item"><a class="nav-link" href="favorites.html">Favorites</a></li>
-      <li class="nav-item"><a class="nav-link" href="letters-to-diana.html">Letters to Diana</a></li>
+      <li class="nav-item"><a class="nav-link" href="letters-to-diana.html">Life Notes</a></li>
       <li class="nav-item"><a class="nav-link" href="video-memories.html">Videos</a></li>
       <li class="nav-item nav-item-year">
         <button class="nav-link nav-link-year" type="button" aria-haspopup="true">2023</button>
